484e4f3947452da03d2e99c298a1ba2f54423e92
nov6m <chernov24680@gmail.com>
2022-03-07 22:22:25 +0300

    add 2 new component (property, feature)

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,15 +1,11 @@
 <template>
-    <MainContainer>
         Blog
-    </MainContainer>
 </template>
 
 
 <script>
-import MainContainer from '../components/MainContainer'
 export default {
     components: {
-        MainContainer,
     }
 }
 </script>
